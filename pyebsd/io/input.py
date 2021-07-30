5635e5d0cf24752269c28b388e2590338dac02d9
Chris <fanchercm@ornl.gov>
2021-07-30 11:31:16 -0400

    merge updated ang read from original repo

--- pyebsd/io/input.py
+++ pyebsd/io/input.py
@@ -16,34 +16,34 @@ def _parse_info_header(line, pattern, dtype=str):
 
 def load_ang_file(fname):
     """
-    Loads a TSL ang file. 
+    Loads a TSL ang file.
     The fields of each line in the body of the TSL ang file are as follows:
 
         phi1 Phi phi2 x y IQ CI ph intensity fit
 
     where:
-    phi1, Phi, phi2 : Euler angles (in radians) in Bunge's notation for 
+    phi1, Phi, phi2 : Euler angles (in radians) in Bunge's notation for
         describing the lattice orientations and are given in radians.
-        A value of 4 is given to each Euler angle when an EBSP could 
-        not be indexed. These points receive negative confidence index 
+        A value of 4 is given to each Euler angle when an EBSP could
+        not be indexed. These points receive negative confidence index
         values when read into an OIM dataset.
     x, y : The horizontal and vertical coordinates of the points in the
         scan, in micrometers. The origin (0,0) is defined as the top-left
         corner of the scan.
-    IQ : The image quality parameter that characterizes the contrast of 
+    IQ : The image quality parameter that characterizes the contrast of
         the EBSP associated with each measurement point.
     CI : The confidence index that describes how confident the software is
         that it has correctly indexed the EBSP, i.e., confidence that the
         angles are correct.
-    ph : The material phase identifier. This field is 0 for single phase 
+    ph : The material phase identifier. This field is 0 for single phase
         OIM scans or 1,2,3... for multi-phase scans.
     intensity : An integer describing the intensity from whichever detector
-        was hooked up to the OIM system at the time of data collection, 
+        was hooked up to the OIM system at the time of data collection,
         typically a forward scatter detector.
-    fit : The fit metric that describes how the indexing solution matches 
+    fit : The fit metric that describes how the indexing solution matches
         the bands detected by the Hough transform or manually by the user.
     footers:
-        In addition there also may be extra sections - such as EDS counts 
+        In addition there also may be extra sections - such as EDS counts
         data.
 
     Parameters
@@ -114,38 +114,66 @@ def load_ang_file(fname):
 
     print('\n{} points read in {:.2f} s'.format(len(data), time.time() - t0))
 
+    # If any of dx, dy, ncols_odd, ncols_even, nrows is None, then guess them from x, y columns
+    if any(v is None for v in [dx, dy, ncols_odd, ncols_even, nrows]):
+        xcoords = np.unique(data.x)
+        ycoords = np.unique(data.y)
+
+        nrows = len(ycoords)
+        # if SqrGrid or RectGrid
+        if grid.lower() != 'hexgrid':
+            ncols_odd = ncols_even = len(xcoords)
+        else:
+            ncols_odd = np.count_nonzero(data.y == ycoords[0])
+            if nrows > 1:
+                ncols_even = np.count_nonzero(data.y == ycoords[1])
+            else:
+                ncols_even = ncols_odd - 1
+
+        dx = data.x[1] - data.x[0]
+        dy = ycoords[1] - ycoords[0]
+
+        print('Grid parameters guessed from x, y columns:')
+        print('  XSTEP:', dx)
+        print('  YSTEP:', dy)
+        print('  NCOLS_ODD:', ncols_odd)
+        print('  NCOLS_EVEN:', ncols_even)
+        print('  NROWS:', nrows)
+        print('')
+
     return ScanData(data, grid, dx, dy, ncols_odd, ncols_even, nrows, header)
+
 
 def load_ctf_file(fname):
     """
-    Loads a Oxford ctf file. 
+    Loads a Oxford ctf file.
     The fields of each line in the body of the TSL ang file are as follows:
 
         phi1 Phi phi2 x y IQ CI ph intensity fit
 
     where:
-    phi1, Phi, phi2 : Euler angles (in radians) in Bunge's notation for 
+    phi1, Phi, phi2 : Euler angles (in radians) in Bunge's notation for
         describing the lattice orientations and are given in radians.
-        A value of 4 is given to each Euler angle when an EBSP could 
-        not be indexed. These points receive negative confidence index 
+        A value of 4 is given to each Euler angle when an EBSP could
+        not be indexed. These points receive negative confidence index
         values when read into an OIM dataset.
     x, y : The horizontal and vertical coordinates of the points in the
         scan, in micrometers. The origin (0,0) is defined as the top-left
         corner of the scan.
-    IQ : The image quality parameter that characterizes the contrast of 
+    IQ : The image quality parameter that characterizes the contrast of
         the EBSP associated with each measurement point.
     CI : The confidence index that describes how confident the software is
         that it has correctly indexed the EBSP, i.e., confidence that the
         angles are correct.
-    ph : The material phase identifier. This field is 0 for single phase 
+    ph : The material phase identifier. This field is 0 for single phase
         OIM scans or 1,2,3... for multi-phase scans.
     intensity : An integer describing the intensity from whichever detector
-        was hooked up to the OIM system at the time of data collection, 
+        was hooked up to the OIM system at the time of data collection,
         typically a forward scatter detector.
-    fit : The fit metric that describes how the indexing solution matches 
+    fit : The fit metric that describes how the indexing solution matches
         the bands detected by the Hough transform or manually by the user.
     footers:
-        In addition there also may be extra sections - such as EDS counts 
+        In addition there also may be extra sections - such as EDS counts
         data.
 
     Parameters
@@ -166,7 +194,7 @@ def load_ctf_file(fname):
         header = []
         nmatches = 0
         for line in f:
-            
+
             # If header
             if 'MAD' in line:
                 break
@@ -208,7 +236,7 @@ def load_ctf_file(fname):
                        delim_whitespace=True, skiprows=(len(header) + 1))
     # Rename the columns
     columns = list(data.columns)
-    
+
     columns[:11] = ['ph', 'x', 'y', 'bands',
                     'error', 'phi1', 'Phi', 'phi2', 'fit', 'IQ', 'intensity']
     data.columns = columns
